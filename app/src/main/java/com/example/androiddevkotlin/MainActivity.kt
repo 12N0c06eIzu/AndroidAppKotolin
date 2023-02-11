69480916afbdc8e179d7a4f12d64f9c7c09ad7eb
いず <ritu6811@gmail.com>
2023-02-12 00:21:21 +0900

    NavBar実装中

--- app/src/main/java/com/example/androiddevkotlin/MainActivity.kt
+++ app/src/main/java/com/example/androiddevkotlin/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.androiddevkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // navFragmentContainerViewの設定
+        setNavViewEvent()
+    }
+
+    private fun setNavViewEvent(){
+        // bottomNavViewのアイテムのIDを取得する。
+        val bottomNavView: BottomNavigationView = findViewById(R.id.bottom_nav)
+        // 設置したnavigationLayoutのフラグメントを取得する。
+//        val navController = findNavController(R.id.navFragmentContainerView)
+
+//        NavigationUI.setupWithNavController(bottomNavView, navController)
     }
 }
--- app/src/main/java/com/example/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/navigation/HomeFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TitleFragment.newInstance] factory method to
+ * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TitleFragment : Fragment() {
+class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,8 @@ class TitleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.placeholder_layout, container, false)
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        return view
     }
 
     companion object {
@@ -50,7 +51,7 @@ class TitleFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TitleFragment().apply {
+            HomeFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
